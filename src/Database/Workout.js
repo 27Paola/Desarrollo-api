3d92f08d7b165f41c9b8cd9a923144a2e5471cbd
Paola Sanchez <yineth2paola2@gmail.com>
2023-02-20 17:31:11 -0500

    conection

--- src/Database/Workout.js
+++ src/Database/Workout.js
@@ -1,7 +1,22 @@
-const DB = require("../Database/Model");
+const DB = require('../models/Series');
+const { saveToDatabase } = require('./utils');
 
 const getAllWorkouts = () => {
     return DB.workouts;
 };
 
-module.exports = {getAllWorkouts};
+const createdWorkout = (newWorkout) => {
+    const isAlreadyAdded =
+        DBMongo.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+
+    if (isAlreadyAdded) {
+        return;
+    }
+
+    DBMongo.workout.push(newWorkout);
+    saveToDatabase(DBMongo);
+    return newWorkout;
+}
+
+
+module.exports = { getAllWorkouts, createdWorkout };
--- src/controllers/SeriesController.js
+++ src/controllers/SeriesController.js
@@ -1,38 +1,59 @@
-const workoutService = require ("../services/SeriesService");
-const Workout = require('../models/Series')
-const SeriesSchema = require ("../models/Series")
+const workoutService = require("../services/SeriesService");
+const Workout = require('../models/Series');
+const SeriesSchema = require("../models/Series");
 
 const getAllWorkouts = (req, res) => {
     Workout.find((err, result) => {
-        if(err) throw new Error(err);
+        if (err) throw new Error(err);
         res.json(result);
+        console.log(result);
     })
 };
 
 const getOneWorkout = (req, res) => {
-    const {SeriesId} = req.params
-    if(!SeriesId) {
+    const { SeriesId } = req.params
+    if (!SeriesId) {
         return;
     }
     Workout.findById(SeriesId, (err, result) => {
-        if(err) throw new Error(err);
+        if (err) throw new Error(err);
         res.json(result);
     })
 };
 
 const createNewWorkout = (req, res) => {
-    const createdWorkout = workoutService.createNewWorkout(req.params.workoutId);
-    res.send('Create workout ' +req.params.workoutId);
+    const { body } = req;
+
+    if (
+        !body.name ||
+        !body.chapter ||
+        !body.chapter_time ||
+        !body.release_date
+    ) {
+        return;
+    }
+
+    const newWorkout = {
+        name: body.name,
+        chapter: body.chapter,
+        chapter_time: body.chapter_time,
+        release_date: body.release_date
+    };
+
+    console.log("newWorkout", newWorkout);
+
+const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.staus(201).send({ status: "OK", data: createdArtist });
 };
 
 const updateOneWorkout = (req, res) => {
     const updatedWorkout = workoutService.updateOneWorkout(req.params.workoutId);
-    res.send('Update workout ' +req.params.workoutId);
+    res.send('Update workout ' + req.params.workoutId);
 };
 
 const deleteOneWorkout = (req, res) => {
     workoutService.deleteOneWorkout(req.params.workoutId);
-    res.send('Delete workout ' +req.params.workoutId);
+    res.send('Delete workout ' + req.params.workoutId);
 };
 
 module.exports = {
